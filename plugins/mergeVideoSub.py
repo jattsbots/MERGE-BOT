f3f9996442db29bea59a2e4fa07c5602a6f7a8ce
Anmol Singh <bxjjjzid@gmail.com>
2024-08-26 11:51:10 +0530

    Update mergeVideoSub.py

--- plugins/mergeVideoSub.py
+++ plugins/mergeVideoSub.py
@@ -116,7 +116,7 @@ async def mergeSub(c: Client, cb: CallbackQuery, new_file_name: str):
         queueDB.update({cb.from_user.id: {"videos": [], "subtitles": [], "audios": []}})
         formatDB.update({cb.from_user.id: None})
         return
-    if file_size > 2044723200 and Config.IS_PREMIUM == False:
+    if file_size > 2044723200 and Config.IS_PREMIUM == True:
         await cb.message.edit(
             f"Video is Larger than 2GB Can't Upload,\n\n Tell {Config.OWNER_USERNAME} to add premium account to get 4GB TG uploads"
         )
