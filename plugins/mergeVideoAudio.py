bd82b9d6de3b3b7c34b5b05f2a1a69a239268931
Anmol Singh <bxjjjzid@gmail.com>
2024-08-26 11:53:29 +0530

    Update mergeVideoAudio.py

--- plugins/mergeVideoAudio.py
+++ plugins/mergeVideoAudio.py
@@ -102,7 +102,7 @@ async def mergeAudio(c: Client, cb: CallbackQuery, new_file_name: str):
         formatDB.update({cb.from_user.id: None})
         return
 
-    if file_size > 2044723200 and Config.IS_PREMIUM == False:
+    if file_size > 2044723200 and Config.IS_PREMIUM == True:
         await cb.message.edit(
             f"Video is Larger than 2GB Can't Upload,\n\n Tell {Config.OWNER_USERNAME} to add premium account to get 4GB TG uploads"
         )
